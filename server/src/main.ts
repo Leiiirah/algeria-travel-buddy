c0191bd7dcbab558934875db8808e7796901a5e9
Mounir Hamzi <mounirhamzi8@gmail.com>
2026-02-01 12:16:40 +0000

    fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-
+app.setGlobalPrefix('api');  // <--- ADD THIS LINE HERE
   // Global exception filter
   app.useGlobalFilters(new HttpExceptionFilter());
 
